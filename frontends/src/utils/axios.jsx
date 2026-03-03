0e980c29bb475fbde8b0da38a748fe2aec0e5f4a
Annu Singh <annu37752@gmail.com>
2026-03-03 08:39:05 +0530

    Frontend ready for deploy

--- frontends/src/utils/axios.jsx
+++ frontends/src/utils/axios.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/admin`;
 
 const instance = axios.create({
   baseURL: API_URL,
 });
-
 
 export default instance;
